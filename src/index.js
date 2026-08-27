c0459ff52ce0d9f1360df4ea38d415151128f577
memesofting <bibitayoadekunle00@gmail.com>
2026-08-27 15:57:47 +0100

    adding cache

--- src/index.js
+++ src/index.js
@@ -5,9 +5,20 @@ const books_url = 'https://books.toscrape.com/'
 
 
 const robot_scrape = async(url) =>{
-    const books = await axios.get(url, {timeout: 6000});
-    console.log(books.data)
-    await fs.writeFile('index.html', books.data, 'utf-8')
+    try {
+        const books = await axios.get(url, {
+            timeout: 6000,
+            headers:{
+                "User-Agent": "FlyRankInternship-A9/1.0 (https://github.com/memesofting/scrapit)"
+            }
+        });
+        if (books.status === 200){
+            // console.log(books.data)
+            await fs.writeFile('cache/catalogue-page-1.html', books.data, 'utf-8')
+        }
+    } catch(error){
+        console.log(error)
+    };
 }
 
 robot_scrape(books_url)
