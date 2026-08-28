1ea2526a2fba95c5c84e7184f4f6c7cbd9c0d791
memesofting <bibitayoadekunle00@gmail.com>
2026-08-28 14:53:45 +0100

    Stage 1: fetch and cache HTML

--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
 import axios from "axios";
-import fs from 'node:fs/promises'
+import fs from 'node:fs'
 
 const books_url = 'https://books.toscrape.com/'
 
+const cachePath = 'cache/catalogue-page-1.html';
 
 const robot_scrape = async(url) =>{
     try {
         const books = await axios.get(url, {
-            timeout: 6000,
+            timeout: 10000,
             headers:{
                 "User-Agent": "FlyRankInternship-A9/1.0 (https://github.com/memesofting/scrapit)"
             }
         });
         if (books.status === 200){
             // console.log(books.data)
-            await fs.writeFile('cache/catalogue-page-1.html', books.data, 'utf-8')
+            await fs.promises.writeFile(cachePath, books.data, 'utf-8')
         }
     } catch(error){
         console.log(error)
     };
 }
 
-robot_scrape(books_url)
+
+const fetchOrReadCache = async()=>{
+    if (fs.existsSync(cachePath)){
+        // read cache
+        console.log('CACHE HIT')
+    }
+    else{
+        console.log('FETCH')
+        robot_scrape(books_url)
+    }
+}
+
+fetchOrReadCache();
